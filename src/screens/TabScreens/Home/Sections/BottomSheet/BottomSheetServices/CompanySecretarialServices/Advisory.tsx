2870ea5028d023d0b282e2cbd2991b545d987776
Nelson Odo <nelsonnedum@gmail.com>
2021-06-03 00:59:17 +0100

    advisory form completed

--- src/screens/TabScreens/Home/Sections/BottomSheet/BottomSheetServices/CompanySecretarialServices/Advisory.tsx
+++ src/screens/TabScreens/Home/Sections/BottomSheet/BottomSheetServices/CompanySecretarialServices/Advisory.tsx
@@ -1,9 +1,11 @@
 import CustomButton from "components/CustomButton";
 import Input from "components/Input";
 import globalStyles from "css/GlobalCss";
+import { useDocUpload } from "hooks/useDocUpload";
 import { ROUTES } from "navigation/Routes";
 import React from "react";
 import { Text, View, ScrollView } from "react-native";
+import { Value } from "react-native-reanimated";
 import modalFormstyles from "../ModalFormStyles";
 
 interface Props {
@@ -12,6 +14,29 @@ interface Props {
 }
 
 export function Advisory({ navigation, closeModal }: Props) {
+  const [formData, setFormData] = React.useState({});
+  const { pickDocument, isUploaded, disabled, docName } = useDocUpload(
+    "signature",
+    "Business Name registration"
+  );
+
+  const handleTextChange = (payload: { field: string; value: string }) => {
+    setFormData((values) => ({
+      ...values,
+      [payload.field]: { key: payload.field, value: payload.value },
+    }));
+  };
+
+  const submit = () => {
+    console.log(formData);
+
+    if (formData == null) {
+    } else {
+      // closeModal();
+      // navigation.navigate(ROUTES.CHECKOUT_SCREEN);
+    }
+  };
+
   return (
     <View style={{ paddingBottom: 120 }}>
       <ScrollView>
@@ -19,62 +44,89 @@ export function Advisory({ navigation, closeModal }: Props) {
         <Text style={modalFormstyles.titleDesc}>
           Please fill the form with your proposed business details
         </Text>
-        <Text style={modalFormstyles.inputLabel}>Business Name</Text>
+        <Text style={modalFormstyles.inputLabel}>
+          Business Name <Text style={modalFormstyles.required}>*</Text>
+        </Text>
         <Input
           placeholder="Type business name"
           errorText={""}
-          onChangeText={(text: string) => null}
+          onChangeText={(text: string) => {
+            handleTextChange({ field: "BusinessName", value: text });
+          }}
         />
         <View style={{ height: 16 }} />
-        <Text style={modalFormstyles.inputLabel}> Business Sector</Text>
+        <Text style={modalFormstyles.inputLabel}>
+          Business Sector <Text style={modalFormstyles.required}>*</Text>
+        </Text>
         <Input
           placeholder="Enter business sector"
           errorText={""}
-          onChangeText={(text: string) => null}
+          onChangeText={(text: string) => {
+            handleTextChange({ field: "BusinessSector", value: text });
+          }}
         />
         <View style={{ height: 16 }} />
-        <Text style={modalFormstyles.inputLabel}>Contract Duration</Text>
+        <Text style={modalFormstyles.inputLabel}>
+          Contract Duration <Text style={modalFormstyles.required}>*</Text>
+        </Text>
         <Input
           placeholder="Select contract duration"
           errorText={""}
-          onChangeText={(text: string) => null}
+          onChangeText={(text: string) => {
+            handleTextChange({ field: "ContractDuration", value: text });
+          }}
         />
         <View style={{ height: 16 }} />
         <Text style={modalFormstyles.inputLabel}>
           Certificate of Registration (SMEs)
+          <Text style={modalFormstyles.required}>*</Text>
         </Text>
         <Input
-          placeholder="Upload certificate of Registration"
-          errorText={""}
-          onChangeText={(text: string) => null}
+          onPress={pickDocument}
+          dataValue={docName}
+          icon
+          onChangeText={(text: string) => {
+            handleTextChange({
+              field: "CertificateOfRegistration",
+              value: text,
+            });
+          }}
         />
         <View style={{ height: 16 }} />
         <Text style={modalFormstyles.inputLabel}>
           Certificate of Incorporation (Companies)
+          <Text style={modalFormstyles.required}>*</Text>
         </Text>
         <Input
-          placeholder="Upload certificate of incorporation"
-          errorText={""}
-          onChangeText={(text: string) => null}
+          onPress={pickDocument}
+          dataValue={docName}
+          icon
+          onChangeText={(text: string) => {
+            handleTextChange({
+              field: "CertificateOfIncorporation",
+              value: text,
+            });
+          }}
         />
         <View style={{ height: 16 }} />
         <Text style={modalFormstyles.inputLabel}>
           Memorandum and Articles of Association
+          <Text style={modalFormstyles.required}> *</Text>
         </Text>
         <Input
-          placeholder="Upload memorandum"
-          errorText={""}
-          onChangeText={(text: string) => null}
+          onPress={pickDocument}
+          dataValue={docName}
+          icon
+          onChangeText={(text: string) => {
+            handleTextChange({
+              field: "MemorandumAndArticlesOfAssociation",
+              value: text,
+            });
+          }}
         />
       </ScrollView>
       <View style={{ height: 16 }} />
-      <CustomButton
-        btnText="Submit"
-        onClick={() => {
-          closeModal();
-          navigation.navigate(ROUTES.CHECKOUT_SCREEN);
-        }}
-      />
+      <CustomButton btnText="Submit" onClick={submit} />
     </View>
   );
 }
