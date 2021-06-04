9123af38c4f98980c5d7753e8a72abdcfcd408cf
Ajulibe <akachi_benji@yahoo.com>
2021-06-04 08:36:44 +0100

    added utility functions to uploadDocsService

--- src/screens/TabScreens/Home/Sections/BottomSheet/BottomSheetServices/CompanySecretarialServices/Advisory.tsx
+++ src/screens/TabScreens/Home/Sections/BottomSheet/BottomSheetServices/CompanySecretarialServices/Advisory.tsx
@@ -11,6 +11,8 @@ import { DocUploadInterface, pickAndUploadFile } from "utils/FileUploads";
 import { Service } from "database/DBData";
 import { LawyerModel } from "models/Interfaces";
 import { confirmUpload, getHistoryId } from "services/UploadDocsService";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { wp } from "utils/Dimensions";
 
 interface Props {
   navigation: any;
@@ -37,6 +39,7 @@ export function Advisory(props: Props) {
 
   const submit = () => {
     console.log(formData);
+    console.log(formData);
 
     if (formData == null) {
     } else {
@@ -46,7 +49,7 @@ export function Advisory(props: Props) {
   };
 
   const uploadFile = async (field: string) => {
-    const historyID = await getHistoryId(service.categoryCode);
+    const historyID = await getHistoryId(service.serviceCode);
     if (historyID == null) {
       console.log("Error initiating the history ID");
       return;
@@ -70,90 +73,92 @@ export function Advisory(props: Props) {
   return (
     <View style={{ paddingBottom: 120 }}>
       <ScrollView>
-        <Text style={globalStyles.H1Style}>Advisory</Text>
-        <Text style={modalFormstyles.titleDesc}>
-          Please fill the form with your proposed business details
-        </Text>
-        <Text style={modalFormstyles.inputLabel}>
-          Business Name <Text style={modalFormstyles.required}>*</Text>
-        </Text>
-        <Input
-          placeholder="Type business name"
-          errorText={""}
-          onChangeText={(text: string) => {
-            handleTextChange({ field: "BusinessName", value: text });
-          }}
-        />
-        <View style={{ height: 16 }} />
-        <Text style={modalFormstyles.inputLabel}>
-          Business Sector <Text style={modalFormstyles.required}>*</Text>
-        </Text>
-        <Input
-          placeholder="Enter business sector"
-          errorText={""}
-          onChangeText={(text: string) => {
-            handleTextChange({ field: "BusinessSector", value: text });
-          }}
-        />
-        <View style={{ height: 16 }} />
-        <Text style={modalFormstyles.inputLabel}>
-          Contract Duration <Text style={modalFormstyles.required}>*</Text>
-        </Text>
-        <Input
-          placeholder="Select contract duration"
-          errorText={""}
-          onChangeText={(text: string) => {
-            handleTextChange({ field: "ContractDuration", value: text });
-          }}
-        />
-        <View style={{ height: 16 }} />
-        <Text style={modalFormstyles.inputLabel}>
-          Certificate of Registration (SMEs)
-          <Text style={modalFormstyles.required}>*</Text>
-        </Text>
-        <Input
-          onPress={() => uploadFile("CertificateOfRegistration")}
-          dataValue={docName}
-          icon
-          onChangeText={(text: string) => {
-            handleTextChange({
-              field: "CertificateOfRegistration",
-              value: text,
-            });
-          }}
-        />
-        <View style={{ height: 16 }} />
-        <Text style={modalFormstyles.inputLabel}>
-          Certificate of Incorporation (Companies)
-          <Text style={modalFormstyles.required}>*</Text>
-        </Text>
-        <Input
-          onPress={pickDocument}
-          dataValue={docName}
-          icon
-          onChangeText={(text: string) => {
-            handleTextChange({
-              field: "CertificateOfIncorporation",
-              value: text,
-            });
-          }}
-        />
-        <View style={{ height: 16 }} />
-        <Text style={modalFormstyles.inputLabel}>
-          Memorandum and Articles of Association
-          <Text style={modalFormstyles.required}> *</Text>
-        </Text>
-        <Input
-          onPress={pickDocument}
-          dataValue={docName}
-          icon
-          onChangeText={(text: string) => {
-            handleTextChange({
-              field: "MemorandumAndArticlesOfAssociation",
-              value: text,
-            });
-          }}
-        />
+        <KeyboardAwareScrollView extraScrollHeight={wp(100)}>
+          <Text style={globalStyles.H1Style}>Advisory</Text>
+          <Text style={modalFormstyles.titleDesc}>
+            Please fill the form with your proposed business details
+          </Text>
+          <Text style={modalFormstyles.inputLabel}>
+            Business Name <Text style={modalFormstyles.required}>*</Text>
+          </Text>
+          <Input
+            placeholder="Type business name"
+            errorText={""}
+            onChangeText={(text: string) => {
+              handleTextChange({ field: "BusinessName", value: text });
+            }}
+          />
+          <View style={{ height: 16 }} />
+          <Text style={modalFormstyles.inputLabel}>
+            Business Sector <Text style={modalFormstyles.required}>*</Text>
+          </Text>
+          <Input
+            placeholder="Enter business sector"
+            errorText={""}
+            onChangeText={(text: string) => {
+              handleTextChange({ field: "BusinessSector", value: text });
+            }}
+          />
+          <View style={{ height: 16 }} />
+          <Text style={modalFormstyles.inputLabel}>
+            Contract Duration <Text style={modalFormstyles.required}>*</Text>
+          </Text>
+          <Input
+            placeholder="Select contract duration"
+            errorText={""}
+            onChangeText={(text: string) => {
+              handleTextChange({ field: "ContractDuration", value: text });
+            }}
+          />
+          <View style={{ height: 16 }} />
+          <Text style={modalFormstyles.inputLabel}>
+            Certificate of Registration (SMEs)
+            <Text style={modalFormstyles.required}>*</Text>
+          </Text>
+          <Input
+            onPress={() => uploadFile("CertificateOfRegistration")}
+            dataValue={docName}
+            icon
+            onChangeText={(text: string) => {
+              handleTextChange({
+                field: "CertificateOfRegistration",
+                value: text,
+              });
+            }}
+          />
+          <View style={{ height: 16 }} />
+          <Text style={modalFormstyles.inputLabel}>
+            Certificate of Incorporation (Companies)
+            <Text style={modalFormstyles.required}>*</Text>
+          </Text>
+          <Input
+            onPress={pickDocument}
+            dataValue={docName}
+            icon
+            onChangeText={(text: string) => {
+              handleTextChange({
+                field: "CertificateOfIncorporation",
+                value: text,
+              });
+            }}
+          />
+          <View style={{ height: 16 }} />
+          <Text style={modalFormstyles.inputLabel}>
+            Memorandum and Articles of Association
+            <Text style={modalFormstyles.required}> *</Text>
+          </Text>
+          <Input
+            onPress={pickDocument}
+            dataValue={docName}
+            icon
+            onChangeText={(text: string) => {
+              handleTextChange({
+                field: "MemorandumAndArticlesOfAssociation",
+                value: text,
+              });
+            }}
+          />
+        </KeyboardAwareScrollView>
       </ScrollView>
       <View style={{ height: 16 }} />
       <CustomButton btnText="Submit" onClick={submit} />
